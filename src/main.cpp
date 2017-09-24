e06b0bb31f8e80435b94f452af00b7a8c6622909
Siddharth Bhat <siddu.druid@gmail.com>
2017-09-25 01:21:48 +0200

    [Codegen] can now codegen print int

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,192 @@
-#include "llvm/ADT/SmallVector.h"
-#include "llvm/IR/IRBuilder.h"
 #include <iostream>
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
 #include "stgir.h"
 
 using namespace std;
+using namespace stg;
+using namespace llvm;
+
+using StgIRBuilder = IRBuilder<>;
 
 // http://web.iitd.ac.in/~sumeet/flex__bison.pdf
 // http://aquamentus.com/flex_bison.html
 //
+//
+
+// Get a function with name `name`, and if it doesn't exist, create
+// a function with name `name`, type `FTy`, module `m`.
+Function *getOrCreateFunction(Module &m, FunctionType *FTy, std::string name) {
+    Function *F = m.getFunction(name);
+    if (F) return F;
+
+    return Function::Create(FTy, GlobalValue::ExternalLinkage, name, &m);
+}
+
+static const int stackSize = 5000;
+struct BuildCtx {
+   private:
+    void buildPushInt(Module &m, StgIRBuilder &builder) {
+        assert(pushInt);
+        assert(stackPrimTop);
+        assert(stackPrim);
+
+        BasicBlock *entry =
+            BasicBlock::Create(m.getContext(), "entry", pushInt);
+        builder.SetInsertPoint(entry);
+        for (Argument &arg : pushInt->args()) {
+            arg.setName("i");
+            Value *idx = builder.CreateLoad(stackPrimTop, "idx");
+        Value *stackSlot = builder.CreateGEP(stackPrim, {builder.getInt64(0), idx}, "slot");
+            builder.CreateStore(&arg, stackSlot);
+            
+            idx = builder.CreateAdd(idx, builder.getInt64(1), "idx_inc");
+            builder.CreateStore(idx, stackPrimTop);
+            builder.CreateRetVoid();
+            
+        }
+    }
+
+    void buildPopInt(Module &m, StgIRBuilder &builder) {
+        assert(popInt);
+        assert(stackPrimTop);
+        assert(stackPrim);
+
+        BasicBlock *entry =
+            BasicBlock::Create(m.getContext(), "entry", popInt);
+        builder.SetInsertPoint(entry);
+
+        
+        Value *idx = builder.CreateLoad(stackPrimTop, "idx");
+        idx = builder.CreateSub(idx, builder.getInt64(1), "idx_dec");
+        Value *stackSlot = builder.CreateGEP(stackPrim, {builder.getInt64(0), idx}, "slot");
+        Value *Ret = builder.CreateLoad(stackSlot, "val");
+
+        builder.CreateStore(idx, stackPrimTop);
+        builder.CreateRet(Ret);
+        
+    }
+
+   public:
+    Function *printInt, *popInt, *pushInt;
+    GlobalVariable *stackPrim;
+    GlobalVariable *stackPrimTop;
+    using BindingMapTy = std::map<Binding *, Function *>;
+    BindingMapTy bindingmap;
+
+    BuildCtx(Module &m, StgIRBuilder &builder) {
+        printInt = getOrCreateFunction(m,
+                                       FunctionType::get(builder.getVoidTy(),
+                                                         /*isVarArg=*/false),
+                                       "printInt");
+        // printInt = getOrCreateFunction(
+        //     m,
+        //     FunctionType::get(builder.getVoidTy(), {builder.getInt64Ty()},
+        //                      /*isVarArg=*/false),
+        //    "printInt");
+
+        popInt = getOrCreateFunction(
+            m, FunctionType::get(builder.getInt64Ty(), /*isVarArg=*/false),
+            "popInt");
+        pushInt = getOrCreateFunction(
+            m,
+            FunctionType::get(builder.getVoidTy(), {builder.getInt64Ty()},
+                              /*isVarArg=*/false),
+            "pushInt");
+        Type *stackPrimTy = ArrayType::get(builder.getInt64Ty(), stackSize);
+        Constant *stackPrimInit = ConstantAggregateZero::get(stackPrimTy);
+        stackPrim = new GlobalVariable(m, stackPrimTy, /*isConstant=*/false,
+                                       GlobalValue::ExternalLinkage,
+                                       stackPrimInit, "stackPrim");
+
+        stackPrimTop = new GlobalVariable(
+            m, builder.getInt64Ty(), /*isConstant=*/false,
+            GlobalValue::ExternalLinkage,
+            ConstantInt::get(builder.getInt64Ty(), 0), "stackPrimTop");
+        buildPushInt(m, builder);
+        buildPopInt(m, builder);
+    }
+
+    Function *getFunctionFromName(std::string name) {
+        if (name == "printInt") return printInt;
+        for (auto It : bindingmap)
+            if (It.first->getName() == name) return It.second;
+        cerr << __PRETTY_FUNCTION__ << " |unknown function with name: " << name
+             << "\n";
+        assert(false && "unknown function");
+    }
+};
+
+Value *materializeAtom(const AtomInt *i, StgIRBuilder &builder) {
+    return builder.getInt64(i->getVal());
+}
+
+void materializeExpr(const ExpressionAp *ap, Module &m, StgIRBuilder &builder,
+                     BuildCtx &bctx) {
+    for (Atom *p : *ap) {
+        AtomInt *i = cast<AtomInt>(p);
+        Value *v = materializeAtom(i, builder);
+        builder.CreateCall(bctx.pushInt, {v});
+
+        Function *Cont = bctx.getFunctionFromName(ap->getFnName());
+        builder.CreateCall(Cont);
+    }
+};
+
+void materializeExpr(const Expression *e, Module &m, StgIRBuilder &builder,
+                     BuildCtx &bctx) {
+    cout << e->getKind() << "|" << *e << "\n";
+    switch (e->getKind()) {
+        case Expression::EK_Ap:
+            materializeExpr(cast<ExpressionAp>(e), m, builder, bctx);
+            break;
+        case Expression::EK_Cons:
+        case Expression::EK_Case:
+            assert(false && "unimplemented");
+            break;
+    };
+}
+
+Function *materializeBinding(const Binding *b, Module &m, StgIRBuilder &builder,
+                             BuildCtx &bctx) {
+    FunctionType *FTy =
+        FunctionType::get(builder.getVoidTy(), /*isVarArg=*/false);
+    Function *F = Function::Create(FTy, GlobalValue::ExternalLinkage,
+                                   b->getName(), &m);
+
+    BasicBlock *Entry = BasicBlock::Create(m.getContext(), "entry", F);
+    builder.SetInsertPoint(Entry);
+    materializeExpr(b->getRhs(), m, builder, bctx);
+    builder.CreateRetVoid();
+    return F;
+}
 
 void compile_program(stg::Program *program) {
     cout << "> program: " << *program << "\n";
+    LLVMContext ctx;
+    StgIRBuilder builder(ctx);
+    Module m("Module", ctx);
+
+    BuildCtx bctx(m, builder);
+
+    Binding *entrystg = nullptr;
+
+    for (Binding *b : *program) {
+        if (b->getName() == "main") {
+            assert(!entrystg && "program has more than one main.");
+            entrystg = b;
+        }
+        bctx.bindingmap[b] = materializeBinding(b, m, builder, bctx);
+    }
+    m.print(errs(), nullptr);
 }
--- src/stgir.h
+++ src/stgir.h
@@ -1,7 +1,7 @@
 #pragma once
-#include <iostream>
-#include <llvm/ADT/SmallVector.h>
 #include <llvm/ADT/ArrayRef.h>
+#include <llvm/ADT/SmallVector.h>
+#include <iostream>
 
 #include <llvm/Support/Casting.h>
 
@@ -13,106 +13,125 @@ using ConstructorName = std::string;
 
 // *** Atom ***
 class Atom {
-public:
-  enum AtomKind { AK_Int, AK_Ident };
+   public:
+    enum AtomKind { AK_Int, AK_Ident };
 
-  AtomKind getKind() const { return kind; }
-  virtual void print(std::ostream &os) const = 0;
+    AtomKind getKind() const { return kind; }
+    virtual void print(std::ostream &os) const = 0;
 
-  friend std::ostream &operator<<(std::ostream &os, const Atom &a);
+    friend std::ostream &operator<<(std::ostream &os, const Atom &a);
 
-protected:
-  Atom(AtomKind kind) : kind(kind){};
+   protected:
+    Atom(AtomKind kind) : kind(kind){};
 
-private:
-  const AtomKind kind;
+   private:
+    const AtomKind kind;
 };
 
-
 class AtomInt : public Atom {
-  int val;
+    int val;
 
-public:
-  AtomInt(int val) : val(val), Atom(Atom::AK_Int) {}
-  void print(std::ostream &os) const ;
-  static bool classof(const Atom *S) { return S->getKind() == Atom::AK_Int; }
+   public:
+    AtomInt(int val) : val(val), Atom(Atom::AK_Int) {}
+    void print(std::ostream &os) const;
+    static bool classof(const Atom *S) { return S->getKind() == Atom::AK_Int; }
+    int getVal() const { return val; }
 };
 
 class AtomIdent : public Atom {
-  Identifier ident;
+    Identifier ident;
 
-public:
-  AtomIdent(std::string ident) : ident(ident), Atom(Atom::AK_Ident) {}
-  void print(std::ostream &os) const;
+   public:
+    AtomIdent(std::string ident) : ident(ident), Atom(Atom::AK_Ident) {}
+    void print(std::ostream &os) const;
 
+    Identifier getIdent() const { return ident; };
 
-  Identifier getIdent() const { return ident; };
-
-  static bool classof(const Atom *S) { return S->getKind() == Atom::AK_Ident; }
+    static bool classof(const Atom *S) {
+        return S->getKind() == Atom::AK_Ident;
+    }
 };
-
 
 // *** Expression ****
 class Expression {
-public:
-  enum ExpressionKind { EK_Ap, EK_Cons, EK_Case };
-  virtual void print(std::ostream &os) const = 0;
+   public:
+    enum ExpressionKind { EK_Ap, EK_Cons, EK_Case };
+    virtual void print(std::ostream &os) const = 0;
+    ExpressionKind getKind() const { return kind; }
 
-  friend std::ostream &operator<<(std::ostream &os, const Expression &e) {
-    e.print(os);
-    return os;
-  }
+    friend std::ostream &operator<<(std::ostream &os, const Expression &e) {
+        e.print(os);
+        return os;
+    }
 
-protected:
-  Expression(ExpressionKind kind) : kind(kind){};
+   protected:
+    Expression(ExpressionKind kind) : kind(kind){};
 
-private:
-  const ExpressionKind kind;
+   private:
+    const ExpressionKind kind;
 };
 
 class ExpressionAp : public Expression {
-  Identifier fn;
-  SmallVector<Atom *, 2> args;
+   public:
+    using ParamsTy = SmallVector<Atom *, 2>;
+    using iterator = ParamsTy::iterator;
+    using const_iterator = ParamsTy::const_iterator;
 
-public:
-  ExpressionAp(Identifier fn, std::initializer_list<Atom *> args)
-      : fn(fn), args(args), Expression(Expression::EK_Ap){};
+    iterator begin() { return args.begin(); }
+    iterator end() { return args.end(); }
 
-  ExpressionAp(Identifier fn, ArrayRef<Atom *> argsref)
-      : fn(fn), Expression(Expression::EK_Ap){
-        for(Atom* arg : argsref)
-          args.push_back(arg);
-      };
+    const_iterator begin() const { return args.begin(); }
+    const_iterator end() const { return args.end(); }
 
-  void print(std::ostream &os) const;
+    ExpressionAp(Identifier fn, std::initializer_list<Atom *> args)
+        : fn(fn), args(args), Expression(Expression::EK_Ap){};
+
+    ExpressionAp(Identifier fn, ArrayRef<Atom *> argsref)
+        : fn(fn), Expression(Expression::EK_Ap) {
+        for (Atom *arg : argsref) args.push_back(arg);
+    };
+    std::string getFnName() const { return fn; }
+
+    void print(std::ostream &os) const;
+
+    static bool classof(const Expression *E) {
+        return E->getKind() == Expression::EK_Ap;
+    }
+
+   private:
+    Identifier fn;
+    ParamsTy args;
+
 };
 
 class ExpressionConstructor : public Expression {
     ConstructorName name;
     SmallVector<Atom *, 2> args;
-    public:
-    ExpressionConstructor(ConstructorName name, std::initializer_list<Atom *>args)
-        : name(name), args(args), Expression(Expression::EK_Cons) {};
 
-  ExpressionConstructor(ConstructorName name, ArrayRef<Atom *> argsref)
-      : name(name), Expression(Expression::EK_Cons){
-        for(Atom* arg : argsref)
-          args.push_back(arg);
-      };
+   public:
+    ExpressionConstructor(ConstructorName name,
+                          std::initializer_list<Atom *> args)
+        : name(name), args(args), Expression(Expression::EK_Cons){};
+
+    ExpressionConstructor(ConstructorName name, ArrayRef<Atom *> argsref)
+        : name(name), Expression(Expression::EK_Cons) {
+        for (Atom *arg : argsref) args.push_back(arg);
+    };
 
     void print(std::ostream &os) const;
+    static bool classof(const Expression *E) {
+        return E->getKind() == Expression::EK_Cons;
+    }
 };
-
 
 // *** Alt ***
 class CaseAlt {
-protected:
+   protected:
     Expression *rhs;
-    CaseAlt(Expression *rhs) : rhs(rhs) {};
-public:
-    Expression *getRHS() {
-        return rhs;
-    }
+    CaseAlt(Expression *rhs) : rhs(rhs){};
+
+   public:
+    Expression *getRHS() { return rhs; }
 
     friend std::ostream &operator<<(std::ostream &os, const CaseAlt &a);
     virtual void print(std::ostream &os) const = 0;
@@ -120,47 +139,68 @@ public:
 
 class CaseAltInt : public CaseAlt {
     AtomInt *lhs;
-    public:
-    CaseAltInt(AtomInt *lhs, Expression *rhs) : CaseAlt(rhs), lhs(lhs) {};
+
+   public:
+    CaseAltInt(AtomInt *lhs, Expression *rhs) : CaseAlt(rhs), lhs(lhs){};
     void print(std::ostream &os) const;
 };
 
 class ExpressionCase : public Expression {
     Atom *scrutinee;
     SmallVector<CaseAlt *, 2> alts;
-    public:
-    ExpressionCase(Atom *scrutinee, ArrayRef<CaseAlt *> altsref) :
-            scrutinee(scrutinee), Expression(Expression::EK_Case) {
-        for(CaseAlt *alt : altsref) {
+
+   public:
+    ExpressionCase(Atom *scrutinee, ArrayRef<CaseAlt *> altsref)
+        : scrutinee(scrutinee), Expression(Expression::EK_Case) {
+        for (CaseAlt *alt : altsref) {
             alts.push_back(alt);
         }
-     }
+    }
 
     void print(std::ostream &os) const;
+    static bool classof(const Expression *E) {
+        return E->getKind() == Expression::EK_Case;
+    }
 };
 
 // *** Binding ***
 class Binding {
-  Identifier lhs;
-  Expression *rhs;
-public:
-  Binding(Identifier lhs, Expression *rhs) : lhs(lhs), rhs(rhs) {};
-  friend std::ostream &operator<<(std::ostream &os, const Binding &b);
+    Identifier lhs;
+    Expression *rhs;
 
+   public:
+    Binding(Identifier lhs, Expression *rhs) : lhs(lhs), rhs(rhs){};
+    friend std::ostream &operator<<(std::ostream &os, const Binding &b);
+    Identifier getName() const { return lhs; }
+    const Expression *getRhs() const { return rhs; }
 };
-
 
 // *** Program ***
 class Program {
-  SmallVector<Binding*, 4> bindings;
-    public:
+   public:
+    Program(ArrayRef<Binding *> bs) {
+        for (Binding *b : bs) bindings.push_back(b);
+    };
 
-  Program(ArrayRef<Binding *> bs) {
-        for(Binding* b : bs)
-          bindings.push_back(b);
-      };
-  
-  friend std::ostream &operator<<(std::ostream &os, const Program &p);
+    using BindingList = SmallVector<Binding *, 4>;
+    using iterator = BindingList::iterator;
+
+    friend std::ostream &operator<<(std::ostream &os, const Program &p);
+
+    iterator begin() { return bindings.begin(); }
+    iterator end() { return bindings.end(); }
+
+    size_t size() const { return bindings.size(); }
+    bool empty() const { return bindings.empty(); }
+
+    const Binding *front() const { return bindings.front(); }
+    Binding *front() { return bindings.front(); }
+
+    const Binding *back() const { return bindings.back(); }
+    Binding *back() { return bindings.back(); }
+
+   private:
+    BindingList bindings;
 };
 
-} // end namespace stg.
+}  // end namespace stg.
