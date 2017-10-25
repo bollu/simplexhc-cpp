7ea8e83d21a4eb2d879013497f5427a62c4a2ca6
Siddharth Bhat <siddu.druid@gmail.com>
2017-10-25 16:16:31 +0200

    [SEQ] implement seq. For this, we need to teach printInt to pick up a continuation if one exists

--- lib/libstg.c
+++ lib/libstg.c
@@ -1,11 +1,19 @@
 #include <stdint.h>
 #include <stdio.h>
 
+typedef void(*Continuation)(void);
+
+typedef struct {
+    Continuation *cont;
+} Closure;
+
+extern void *stackReturn[5000];
+extern uint64_t stackReturnTop;
+
 extern int64_t popInt();
 extern void pushInt(int64_t);
 
-void printInt() {
-    int64_t i = popInt();
+
+void printOnlyInt(int64_t i) {
     printf("%d", i);
 }
-
