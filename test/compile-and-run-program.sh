684499d17daacdec111388bf4ab52834cec3c060
Siddharth Bhat <siddu.druid@gmail.com>
2017-10-29 03:08:35 +0100

    [TESTS] teach tests about JIT to simplify test scripts

--- test/compile-and-run-program.sh
+++ test/compile-and-run-program.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
 set -e
-SIMPLEXHC=../build/simplexhc
+SIMPLEXHC=../build/sxhc
 LIBSTGPATH=../build/
 LLC=llc
 CC=clang
 
-OUTLL=$1".out.ll"
 OUTO=$1".out.o"
 
-$SIMPLEXHC < $1  $OUTLL >/dev/null
-ret=$?
-if [ $ret -ne 0 ]; then
-    cat $OUTLL
-    rm $OUTLL
-else
-    $LLC $OUTLL -filetype=obj -o $OUTO
-    rm $OUTLL
-    $CC $OUTO -L $LIBSTGPATH -lstgruntime -o $1.out
-    rm $OUTO
-fi;
+$SIMPLEXHC  $1 -o $OUTO
+$CC $OUTO -L $LIBSTGPATH -lstgruntime -o $1.out
+rm $OUTO
 
+# If this is at the CWD, then we need to execute it with `./` prepended
+# to the path. Otherwise, we can directly execute it.
 if [  $(dirname $1)  == $(dirname $0) ]; then
     # need to figure out how to set STG programs's exit code :)
     ./$1.out || true
